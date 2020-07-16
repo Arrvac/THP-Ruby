7035c07e5627111eb9df647ec9f0e40001003d1f
Nico <arrvac@gmail.com>
2020-07-17 01:02:45 +0200

    ici

--- J00/exo_17.rb
+++ J00/exo_17.rb
@@ -1,14 +1,12 @@
 print '> '
 user_number = gets.chomp.to_i
 
-a = 0
 
-a.upto user_number do
+0.upto user_number do |a|
 	if a == user_number - a
 		puts "Il y a #{user_number - a} ans, tu avais la moitie de l'age que tu as aujourd'hui"
 	else
 		puts "Il y a #{user_number - a} ans, tu avais #{a} ans"
 	end
-	a += 1 
 end
 
